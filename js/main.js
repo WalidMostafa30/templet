151d99af63a48c808d9175aa8145f99452940c9b
walidmostafa10 <111182017+walidmostafa10@users.noreply.github.com>
2024-02-08 17:04:14 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let navBtn = document.querySelector(".Navbar__brg-icon");
+let NavbarLinks = document.querySelector(".Navbar__links");
+let NavbarLink = document.querySelectorAll(".Navbar__link");
+
+navBtn.addEventListener("click", () => {
+  NavbarLinks.classList.toggle("open");
+  navBtn.classList.toggle("open");
+});
+NavbarLink.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.target.parentElement.querySelectorAll(".active").forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    link.classList.add("active");
+  });
+});
